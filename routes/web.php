8b6b6453a36dc39fcf5bbe9208ea3eb5c2f88dbd
yashi-yashi09 <k.m.ak04129@gmail.com>
2024-12-04 22:38:42 +0900

    管理者側の認証を実装 修正3

--- routes/web.php
+++ routes/web.php
@@ -24,6 +24,6 @@ require __DIR__.'/auth.php';
 
 Route::group(['prefix' => 'admin', 'as' => 'admin.', 'middleware' => 'auth:admin'], function () {
     Route::get('home', [Admin\HomeController::class, 'index'])->name('home');
-    Route::get('users', [Admin\UserController::class, 'index'])->name('admin.users.index'); // 会員一覧ページ
+    Route::get('users', [Admin\UserController::class, 'index'])->name('users.index'); // 会員一覧ページ
     Route::get('users', [Admin\UserController::class, 'show'])->name('users.show'); 
 });
